d9a8e97b0598b0068c8ad5eec24a85a7456d5236
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-26 14:59:05 -0500

    Exercise completed

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise
+{
+    public class Car : IVehicle, ICompany
+    {
+        //In each of your car, truck, and suv classes
+
+        /*Create 2 members that are specific to each class
+         * Example: truck has a bed size while car has a trunk while suv has a cargo hold size
+         *
+         * Then, Set each class to inherit from both IVehicle and ICompany and implement their members.
+         * 
+         */
+
+        public string CoName { get; set; }
+        public string Logo { get; set; }
+        public bool IsElectric { get; set; }
+        public int TrunkSpace { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int WheelCount { get; set; }
+    }
+}
